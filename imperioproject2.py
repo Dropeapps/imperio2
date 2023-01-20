70d7426c3b0970a5e2fd6f3fa0a1ebca8c2f69a6
Dropeapps <123127131+Dropeapps@users.noreply.github.com>
2023-01-20 20:51:40 -0300

    Update imperioproject2.py

--- imperioproject2.py
+++ imperioproject2.py
@@ -2,45 +2,64 @@ import streamlit as st
 
 st.title(' Cardápio Online ')
 st.subheader('PIZZAS PROMOCIONAIS:')
-st.markdown('•CALABRESA: ').upper() st.markdown('MOLHO DE TOMATE, CALABRESA, CEBOLA E ORÉGANO $32,99').lower()
+st.markdown('•CALABRESA: ').upper() 
+st.markdown('MOLHO DE TOMATE, CALABRESA, CEBOLA E ORÉGANO $32,99').lower()
 
-st.markdown('•MUSSARELA: ').upper() ('MOLHO DE TOMATE, MUSSARELA E ORÉGANO $32,99').lower()
+st.markdown('•MUSSARELA: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA E ORÉGANO $32,99').lower()
 
-st.markdown('•CALABRESA II: ').upper() ('MOLHO DE TOMATE, MUSSARELA, CALABRESA, CEBOLA E ORÉGANO $35,99').lower()
+st.markdown('•CALABRESA II: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, CALABRESA, CEBOLA E ORÉGANO $35,99').lower()
 
-st.markdown('•BAIANA: ').upper() ('MOLHO DE TOMATE, CALABRESA, OVO, CEBOLA, MOLHO DE PIMENTA, MUSSARELA, E ORÉGANO $35,99').lower()
+st.markdown('•BAIANA: ').upper() 
+st.markdown('MOLHO DE TOMATE, CALABRESA, OVO, CEBOLA, MOLHO DE PIMENTA, MUSSARELA, E ORÉGANO $35,99').lower()
 
-st.markdown('•MARGUERITA: ').upper() ('MOLHO DE TOMATE, MUSSARELA, MANJERICÃO E ORÉGANO  $35,99').lower()
+st.markdown('•MARGUERITA: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, MANJERICÃO E ORÉGANO  $35,99').lower()
 
 st.subheader(' PIZZAS TRADICIONAIS R$39,99')
-st.markdown('•ATUM: ').upper() ('MOLHO DE TOMATE, MUSSARELA, ATUM, CEBOLA, AZEITE E ORÉGANO').lower()
+st.markdown('•ATUM: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, ATUM, CEBOLA, AZEITE E ORÉGANO').lower()
 
-st.markdown('•ALHO: ').upper() ('MOLHO DE TOMATE, MUSSARELA, TOMATE FATIADO, AOLHO FRITO E ORÉGANO').lower()
+st.markdown('•ALHO: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, TOMATE FATIADO, AOLHO FRITO E ORÉGANO').lower()
 
-st.markdown('•BACON: ').upper() ('MOLHO DE TOMATE, MUSSARELA, TOMATE FATIADO, CEBOLA, BACON E ORÉGANO').lower()
+st.markdown('•BACON: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, TOMATE FATIADO, CEBOLA, BACON E ORÉGANO').lower()
 
-st.markdown('•BROCOLIS: ').upper() ('MOLHO DE TOMATE, MUSSARELA, BRÓCOLIS, BACON E ORÉGANO').lower()
+st.markdown('•BROCOLIS: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, BRÓCOLIS, BACON E ORÉGANO').lower()
 
-st.markdown('•DOIS QUEJIOS: ').upper() ('MOLHO DE TOMATE, MUSSARELA, CATUPIRY E ORÉGANO').lower()
+st.markdown('•DOIS QUEJIOS: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, CATUPIRY E ORÉGANO').lower()
 
-st.markdown('•MILHO: ').upper() ('MOLHO DE TOMATE, MILHO VERDE, MUSSARELA, TOMATE FATIADO E ORÉGANO').lower()
+st.markdown('•MILHO: ').upper() 
+st.markdown('MOLHO DE TOMATE, MILHO VERDE, MUSSARELA, TOMATE FATIADO E ORÉGANO').lower()
 
-st.markdown('•ESCAROLA: ').upper() ('MOLHO DE TOMATE, MUSSARELA, ESCAROLA REFOGADA, BACON E ORÉGANO').lower()
+st.markdown('•ESCAROLA: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, ESCAROLA REFOGADA, BACON E ORÉGANO').lower()
 
-st.markdown('•FRANGO COM CATUPIRY: ').upper() ('MOLHO DE TOMATE, CATUPIRY, FRANGO DESFIADO, E ORÉGANO').lower()
+st.markdown('•FRANGO COM CATUPIRY: ').upper() 
+st.markdown('MOLHO DE TOMATE, CATUPIRY, FRANGO DESFIADO, E ORÉGANO').lower()
 
-st.markdown('•PALMITO: ').upper() ('MOLHO DE TOMATE, MUSSARELA, PALMITO E ORÉGANO').lower()
+st.markdown('•PALMITO: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, PALMITO E ORÉGANO').lower()
 
-st.markdown('•LOMBO CANADENSE: ').upper() ('MOLHO DE TOMATE, MUSSARELA, LOMBO CANADENSE, CEBOLA, BACON E ORÉGANO').lower()
+st.markdown('•LOMBO CANADENSE: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, LOMBO CANADENSE, CEBOLA, BACON E ORÉGANO').lower()
 
 st.subheader(' PIZZAS ESPECIAIS R$42,99')
-st.markdown('•A MODA: ').upper() ('MOLHO DE TOMATE, MUSSARELA, CATUPIRY, PEPERONI E ORÉGANO').lower()
+st.markdown('•A MODA: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, CATUPIRY, PEPERONI E ORÉGANO').lower()
 
-st.markdown('•BIG MAC: ').upper() ('8 HMBURGUERES, MUSSARELA, MOLHO ESPECIAL, CEBOLA, PICLES E GERGELIM ').lower()
+st.markdown('•BIG MAC: ').upper() 
+st.markdown('8 HMBURGUERES, MUSSARELA, MOLHO ESPECIAL, CEBOLA, PICLES E GERGELIM ').lower()
 
-st.markdown('•IMPÉRIO: ').upper() ('MOLHO DE TOMATE, CARNE MOIDA, CEBOLA, PIMENTÃO, CATUPIRY E ORÉGANO').lower()
+st.markdown('•IMPÉRIO: ').upper() 
+st.markdown('MOLHO DE TOMATE, CARNE MOIDA, CEBOLA, PIMENTÃO, CATUPIRY E ORÉGANO').lower()
 
-st.markdown('•PORTUGUESA: ').upper() ('MOLHO DE TOMATE, MUSSARELA, PRESUNTO, OVOS, CEBOLA, AZEITONA, ERVILHA E ORÉGANO').lower()
+st.markdown('•PORTUGUESA: ').upper() 
+st.markdown('MOLHO DE TOMATE, MUSSARELA, PRESUNTO, OVOS, CEBOLA, AZEITONA, ERVILHA E ORÉGANO').lower()
 
 st.subheader('PIZZAS DOCES R$39,99')
 st.markdown('''
