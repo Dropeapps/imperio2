ae2f08f26006f305c6fc4616bbcab92441190b55
Dropeapps <123127131+Dropeapps@users.noreply.github.com>
2023-01-21 11:22:07 -0300

    Add files via upload

---
+++ imperioproject2.py
@@ -0,0 +1,56 @@
+import streamlit as st
+
+st.title(' Cardápio Online ')
+st.subheader('PIZZAS PROMOCIONAIS:')
+st.markdown('''•CALABRESA: molho de tomate, calabresa cebola e orégano $32,99 
+
+•MUSSARELA: molho de tomate, mussarela e orégano $32,99
+
+•CALABRESA II: molho de tomate, mussarela, calabresa, cebola e orégano $35,99'
+
+•BAIANA: molho de tomate, calabresa, ovo, cebola, molho de pimenta, mussarela, e orégano $35,99
+
+•MARGUERITA: molho de tomate, mussarela, majericão e orégano  $35,99''')
+
+st.subheader(' PIZZAS TRADICIONAIS R$39,99')
+st.markdown('''•ATUM: molho de tomate, mussarela, atum, cebola, azeite e orégano
+
+•ALHO: molho de tomate, mussarela, tomate fatiado, alho frito e orégano
+
+•BACON: molho de tomate, mussarela, tomate fatiado, cebola, bacon e orégano
+
+•BROCOLIS: molho de tomate, mussarela, brócolis, bacon e orégano
+
+•DOIS QUEJIOS: molho de tomate, mussarela, catupiry e orégano
+
+•MILHO: molho de tomate , milho verde, mussarela, tomate fatiado e orégano
+
+•ESCAROLA: molho de tomate, mussarela, escarola refogada, bacon e orégano
+
+•FRANGO COM CATUPIRY: molho de tomate, catupiry, frango desfiado, e orégano
+
+•PALMITO: molho de tomate, mussarela, palmito e orégano
+
+•LOMBO CANADENSE: molho de tomate, mussarela, lombo canadense, cebola, bacon e orégano''')
+
+st.subheader(' PIZZAS ESPECIAIS R$42,99')
+st.markdown('''•A MODA: molho de tomate, mussarela, catupiry, peperoni e orégano
+
+•BIG MAC: 8 hamburgueres, mussarela, molho especial, cebola, picles e gergilim
+
+•IMPÉRIO: molho de tomate, carne moída, cebola, pimentão, catupiry e orégano
+
+•PORTUGUESA: molho de tomate, mussarela, presunto, ovos, cebola, azeitona, ervilha e orégano''')
+
+st.subheader('PIZZAS DOCES R$39,99')
+st.markdown('''
+•BANANA COM MUSSARELA
+
+•M&M'S
+
+•PRESTIGIO
+
+•ROMEU COM JULIETA''')
+
+
+
